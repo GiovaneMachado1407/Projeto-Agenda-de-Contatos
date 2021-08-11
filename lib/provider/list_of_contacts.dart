3959b6399567db16c35befcf64f83d3ce4fe4084
Giovane Machado <giovane1407@gmail.com>
2021-08-11 20:02:58 -0300

    projeto

---
+++ lib/provider/list_of_contacts.dart
@@ -0,0 +1,56 @@
+import 'package:agenda_de_contatos/models/contact.dart';
+
+List<Contact> listOfContacts = [
+  Contact(
+    name: "Nicole Costa",
+    photo: "assets/images/Nicole.jpg",
+    phone: "+55 51 91111-1111",
+    email: "nicole.costa@gmail.com",
+  ),
+  Contact(
+    name: "Camila Santos",
+    photo: "assets/images/Camila.jpg",
+    phone: "+55 98 92222-2222",
+    email: "camila.santos@gmail.com",
+    isFavorite: true,
+  ),
+  Contact(
+    name: "Paulo Alibabas",
+    photo: "assets/images/Paulo.jpg",
+    phone: "+55 51 93333-3333",
+    email: "paulo.ali@gmail.com",
+  ),
+  Contact(
+    name: "Lucas Pinto",
+    photo: "assets/images/Lucas.jpg",
+    phone: "+55 51 94444-4444",
+    email: "lucas.pinto@gmail.com",
+    isFavorite: true,
+  ),
+  Contact(
+    name: "Juliana Paz",
+    photo: "assets/images/Juliana.jpg",
+    phone: "+55 54 95555-555",
+    email: "juliana.paz@gmail.com",
+  ),
+  Contact(
+    name: "Thomas Lomba",
+    photo: "assets/images/Thomas.jpg",
+    phone: "+55 41 96666-6666",
+    email: "thomas.lomba@gmail.com",
+    isFavorite: true,
+  ),
+  Contact(
+    name: "Claudia Silva",
+    photo: "assets/images/Claudia.jpg",
+    phone: "+55 71 97777-7777",
+    email: "claudia.si@gmail.com",
+  ),
+  Contact(
+    name: "Marina Regina",
+    photo: "assets/images/Marina.jpg",
+    phone: "+55 48 98888-8888",
+    email: "mari.re@gmail.com",
+    isFavorite: true,
+  ),
+];
